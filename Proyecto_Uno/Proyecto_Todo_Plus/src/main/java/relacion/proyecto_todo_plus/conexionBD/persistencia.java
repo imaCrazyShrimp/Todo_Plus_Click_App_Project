d3dcedbac4b7274ee9f649414097fb5fa60e726a
Lisandro Barreno <victorbarrenog3@gmail.com>
2026-04-09 23:35:50 -0600

    Agregar packages faltantes

---
+++ Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/conexionBD/persistencia.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package relacion.proyecto_todo_plus.conexionBD;
+
+/**
+ *
+ * @author USER
+ */
+public class persistencia {
+    // borre la bd
+}
