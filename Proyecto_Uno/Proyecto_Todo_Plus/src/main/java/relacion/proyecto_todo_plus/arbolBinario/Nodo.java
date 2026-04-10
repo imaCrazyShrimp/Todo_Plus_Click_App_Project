7b802e2c3df25c1f6001d09af63be99f0f161749
Lisandro Barreno <victorbarrenog3@gmail.com>
2026-04-09 23:29:13 -0600

    Creacion de packages para el proyecto

---
+++ Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/arbolBinario/Nodo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package relacion.proyecto_todo_plus.arbolBinario;
+
+/**
+ *
+ * @author USER
+ */
+public class Nodo {
+    // Aqui va un nodo
+}
