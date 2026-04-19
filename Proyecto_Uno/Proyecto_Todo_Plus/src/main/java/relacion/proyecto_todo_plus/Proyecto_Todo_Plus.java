85941b41c053f27ef470fc399399afc71d9baed5
ecr262678 <calderonedner@gmail.com>
2026-04-18 23:16:28 -0600

    se agrego nuevos cambios de la interface

--- Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/Proyecto_Todo_Plus.java
+++ Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/Proyecto_Todo_Plus.java
@@ -5,6 +5,7 @@ import relacion.proyecto_todo_plus.Interfaz.Principal;
 public class Proyecto_Todo_Plus {
 
     public static void main(String[] args) {
+        //inicio pantalla
      java.awt.EventQueue.invokeLater(() -> new Principal().setVisible(true));   
     }
 }
