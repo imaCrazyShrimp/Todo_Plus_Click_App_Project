841e59d8a86788a2a4dd0b301d59000b1c69e8dc
ecr262678 <calderonedner@gmail.com>
2026-04-18 23:11:45 -0600

    nueva interface corregida

--- Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/Proyecto_Todo_Plus.java
+++ Proyecto_Uno/Proyecto_Todo_Plus/src/main/java/relacion/proyecto_todo_plus/Proyecto_Todo_Plus.java
@@ -1,8 +1,10 @@
 package relacion.proyecto_todo_plus;
 
+import relacion.proyecto_todo_plus.Interfaz.Principal;
+
 public class Proyecto_Todo_Plus {
 
     public static void main(String[] args) {
-        
+     java.awt.EventQueue.invokeLater(() -> new Principal().setVisible(true));   
     }
 }
